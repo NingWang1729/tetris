10551066342fd8de33dfcec5e33d510a9377abb2
Ning Wang <ningwang1729@gmail.com>
2020-10-21 19:53:05 -0700

    Added basic framework for backend

---
+++ src/screens/forum.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from 'react';
+
+function forum() {
+    return (
+        <React.Fragment>
+            <p>div</p>
+        </React.Fragment>
+    );
+}
+
+export default forum;
