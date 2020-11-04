05288b2dffcf136c1c962cbe1917b91d0ebd168c
Adam <admuzzarelli@gmail.com>
2020-11-03 18:02:22 -0800

    Can create new threads dynamically

--- src/screens/forum.js
+++ src/screens/forum.js
@@ -3,43 +3,64 @@ import '../styles/forum.css';
 
 function Forum() {
     /*TO DO:
-        -method to allow user to decide thread title and content instead of static test example
         -method to filter threads based on likes or some other metrics
         -threads state needs to pull all stored threads from server database
     */
 
 
     //creates a state that holds an array of all threads
-    var [threads, setThreads] = useState([]);
-    //var [newThreadName, setNewThreadName] = useState(null);
-    //var [newThreadMessage, setNewThreadMessage] = useState(null);
+    const [threads, setThreads] = useState([]);
 
     //method to add a thread to threads array
-    function addThread(name, message) {
+    function addThread(name, message, setName, setMessage) {
         let newThread = <Thread name={name} message={message}/>;
         setThreads(threads.concat([newThread]));
+        setName(''); //resets input field for name of thread to be place-holder text 
+        setMessage(''); //same but for message of thread
     }
 
     return (
         <React.Fragment>
-            <button className="create-thread-btn" onClick={()=> addThread('test', 'this is a test')}>
-                Create New Thread
-            </button>
+            <CreateNewThread createThread={addThread}/>
             <Thread name="Adam Muzzarelli's First Thread" message="Mr. Yang I love your Tetris game"/>
             {threads}
         </React.Fragment>
     );
 }
 
-/*function CreateNewThread(props) {
+function CreateNewThread(props) {
+    /*TO DO:
+        -prevent creation of thread unless name and message is given a non empty string value
+     */
+
+    const [name, setName] = useState(''); //state to store name of thread
+    const [message, setMessage] = useState(''); //state to store message of thread
+
+    /*returns 2 input fields where user can input information about thread
+    and a button that calls method to create a thread*/
     return (
         <div>
-            <input type="text" placeholder="Thread Name" className="create-thread-input"/>
-            <input type="text" placeholder="Thread Message" className="create-thread-input"/>
-            <button className="create-thread-btn">Create New Thread</button>
+            <input 
+                value={name}
+                onChange={e => setName(e.target.value)}
+                placeholder="Thread Name"
+                className="create-thread-input"
+            />
+            <input
+                value={message}
+                onChange={e => setMessage(e.target.value)}
+                placeholder="Thread Message"
+                className="create-thread-input"
+            />
+            <button
+                onClick={() => props.createThread(name, message, setName, setMessage)}
+                className="create-thread-btn"
+            >
+                Create New Thread
+            </button>
         </div>
     );
-}*/
+}
 
 function Thread(props) {
     /*TO DO:
