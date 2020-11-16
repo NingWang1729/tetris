68daca253700715f1aa8f332014d5b72bae40c5e
Ning Wang <ningwang1729@gmail.com>
2020-11-15 22:04:26 -0800

    edited home page

--- src/screens/home.js
+++ src/screens/home.js
@@ -6,8 +6,8 @@ function Home(port_to_backend) {
         <React.Fragment>
             <h1>
                 <p>Hello World!</p>
-                <p>Please use /tetris for tetris and /forum for forum</p>
-                <p>Current port is: {BACKEND_PORT}</p>
+                <p>Please use /tetris for tetris and /forum for forum in url</p>
+                <p>Current backend port is: {BACKEND_PORT}</p>
             </h1>
         </React.Fragment>
     );
