d6f8dd033107c53d7f3de268faad0e8499aed1d5
Ning Wang <ningwang1729@gmail.com>
2020-11-15 22:03:23 -0800

    added a temporary homepage

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import React from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
 import './App.css';
+import Home from './screens/home';
 import Tetris from './screens/tetris';
-import Forum from './screens/forum'
+import Forum from './screens/forum';
 
 // To be updated every 8 hours until a permanent backend port is found
 const BACKEND_PORT = "https://d05722dfdb2e.ngrok.io";
 
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          header
-        </p>
-      </header>
-      <BrowserRouter>
-        <Route path="/tetris" exact={true} component={Tetris.bind(this, BACKEND_PORT)}/>
-        <Route path="/forum" exact={true} component={Forum.bind(this, BACKEND_PORT)}/>
-      </BrowserRouter>
-      <footer>
-        <p>
-          footer
-        </p>
-      </footer>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <p>
+                    header
+                </p>
+            </header>
+            <BrowserRouter>
+                <Route path="/" exact={true} component={Home.bind(this, BACKEND_PORT)}/>
+                <Route path="/tetris" exact={true} component={Tetris.bind(this, BACKEND_PORT)}/>
+                <Route path="/forum" exact={true} component={Forum.bind(this, BACKEND_PORT)}/>
+            </BrowserRouter>
+            <footer>
+                <p>
+                    footer
+                </p>
+            </footer>
+        </div>
+    );
 }
 
 export default App;
