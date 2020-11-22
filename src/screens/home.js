658fba818c901c6441621201e66ee2b2fa3a90fd
Ning Wang <ningwang1729@gmail.com>
2020-11-21 23:05:12 -0800

    minor formatting improvements to homepage

--- src/screens/home.js
+++ src/screens/home.js
@@ -4,6 +4,10 @@ function Home(port_to_backend) {
     const BACKEND_PORT = port_to_backend;
     return (
         <React.Fragment>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
             <h1>
                 <p>Hello World!</p>
                 <p>Please use /tetris for tetris and /forum for forum in url</p>
