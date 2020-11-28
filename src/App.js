60e2c2208249bcaa95710e4fb48d1a73ec0231da
Ning Wang <ningwang1729@gmail.com>
2020-11-28 12:10:00 -0800

    port maintenance

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Forum from './screens/forum';
 import About from './screens/about';
 
 // To be updated every 8 hours until a permanent backend port is found
-const BACKEND_PORT = "https://78212ef0f5d9.ngrok.io";
+const BACKEND_PORT = "https://cd12fed797e6.ngrok.io";
 
 function App() {
     return (
